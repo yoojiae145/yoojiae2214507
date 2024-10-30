e6fc18d8644cd935459fff40965b3574cfb9d23b
yoojiae145 <chenyback@naver.com>
2024-10-30 14:09:27 +0900

    week09

---
+++ week09/week09-1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	int flag=1;
+	int y;
+	
+	while(flag !=0)
+	{
+		int y;
+		y=3;
+		flag=0;
+	}
+	y=4;
+	
+	return 0;
+}
---
+++ week09/week09-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int all_files;
+extern void sub();
+
+int main(void) {
+	sub();
+	printf("%d\n", all_files);
+	return 0;
+}
+
+extern int all_files;
+void sub(void)
+{
+	all_files=10;
+}
