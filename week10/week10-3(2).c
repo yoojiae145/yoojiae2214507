ad2116b2a1c41be727f61ab90ea5c2d1f3513b97
yoojiae145 <chenyback@naver.com>
2024-11-06 13:53:12 +0900

    week10

---
+++ week10/week10-3(2).c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 5
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i;
+	int a[SIZE]={1,2,3,4,5};
+	int b[SIZE]={1,2,3,7,10};
+	int flag_same=1;
+	
+	for (i=0; i<SIZE; i++)
+	{
+		if(a[i]!=b[i])
+		{
+			printf("%i 배열은 다른 값을 가집니다.\n", i);
+			flag_same=0;
+		}
+	}
+	return 0;
+}
---
+++ week10/week10-5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ROWS 3
+#define COLS 3
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void addMatrix(int A[ROWS][COLS], int B[ROWS][COLS], int C[ROWS][COLS]) {
+	int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void printMatrix(int matrix[ROWS][COLS]) {
+	int i, j;
+    for (i = 0; i < ROWS; i++) {
+        for (j = 0; j < COLS; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main(void) {
+	
+	int A[ROWS][COLS]={ 
+		{2,3,0},
+		{8,9,1},
+		{7,0,5}};
+	int B[ROWS][COLS]={ 
+		{1,0,0},
+		{0,1,0},
+		{0,0,1}};
+	int C[ROWS][COLS];
+	
+	addMatrix(A,B,C);
+	printMatrix(C);
+		
+	return 0;
+}
